f3a245c924721e63027a9d22f50794989a31f881
Aleksandar Vacic <av@codeaplus.com>
2015-07-13 17:25:35 +0200

    Clean up, preparation for the actual code

---
+++ RTCoreDataStack/RTCoreDataManager.h
@@ -0,0 +1,14 @@
+//
+//  RTCoreDataManager.h
+//  RTCoreDataStack
+//
+//  Created by Aleksandar Vacić on 13.7.15..
+//  Copyright (c) 2015. Radiant Tap. All rights reserved.
+//
+
+@import Foundation;
+@import CoreData;
+
+@interface RTCoreDataManager : NSObject
+
+@end
---
+++ RTCoreDataStack/RTCoreDataManager.m
@@ -0,0 +1,13 @@
+//
+//  RTCoreDataManager.m
+//  RTCoreDataStack
+//
+//  Created by Aleksandar Vacić on 13.7.15..
+//  Copyright (c) 2015. Radiant Tap. All rights reserved.
+//
+
+#import "RTCoreDataManager.h"
+
+@implementation RTCoreDataManager
+
+@end
