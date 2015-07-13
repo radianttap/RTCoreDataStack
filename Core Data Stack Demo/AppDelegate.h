3717e0ce362692be071975ee26fd9e2bff8e10e1
Aleksandar Vacic <av@codeaplus.com>
2015-07-13 18:25:31 +0200

    Removed tests target First batch stuff added

--- Core Data Stack Demo/AppDelegate.h
+++ Core Data Stack Demo/AppDelegate.h
@@ -7,18 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <CoreData/CoreData.h>
+
+@class RTCoreDataManager;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
+@property (nonatomic, strong) UIWindow *window;
+@property (nonatomic, strong, readonly) RTCoreDataManager *coreDataManager;
 
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
+//@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+//@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+//@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+//
+//- (void)saveContext;
+//- (NSURL *)applicationDocumentsDirectory;
 
 
 @end
--- Core Data Stack Demo/AppDelegate.m
+++ Core Data Stack Demo/AppDelegate.m
@@ -7,8 +7,11 @@
 //
 
 #import "AppDelegate.h"
+#import "RTCoreDataManager.h"
 
 @interface AppDelegate ()
+
+@property (nonatomic, strong, readwrite) RTCoreDataManager *coreDataManager;
 
 @end
 
@@ -16,40 +19,51 @@
 
 - (BOOL)application:(UIApplication *)application willFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
+	//	start this process as early as possible
+	self.coreDataManager = [[RTCoreDataManager alloc] initWithCallback:^{
+		[self processCoreDataCallback];
+	}];
+
+
+	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+	self.window.backgroundColor = [UIColor whiteColor];
+
+//	ViewController *vc = [[ViewController alloc] init];
+//	UINavigationController *nc = [[UINavigationController alloc] initWithRootViewController:vc];
+//	self.window.rootViewController = nc;
+
 	return YES;
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-	// Override point for customization after application launch.
+
+	[self.window makeKeyAndVisible];
+
 	return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-	// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+
+	[self.coreDataManager save];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-	// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+	[self.coreDataManager save];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-	// Saves changes in the application's managed object context before the application terminates.
-	[self saveContext];
+
+	[self.coreDataManager save];
 }
 
-#pragma mark - Core Data stack
+#pragma mark - Core Data callbacks
 
+- (void)processCoreDataCallback {
+
+}
+
+/*
 @synthesize managedObjectContext = _managedObjectContext;
 @synthesize managedObjectModel = _managedObjectModel;
 @synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
@@ -127,5 +141,5 @@
         }
     }
 }
-
+*/
 @end
