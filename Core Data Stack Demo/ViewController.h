f0ed96f812f1cf58626b4d91b05535d851422087
Aleksandar Vacic <av@codeaplus.com>
2015-07-13 17:21:33 +0200

    Initial demo setup

---
+++ Core Data Stack Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Core Data Stack Demo
+//
+//  Created by Aleksandar Vacić on 13.7.15..
+//  Copyright (c) 2015. Radiant Tap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Core Data Stack Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Core Data Stack Demo
+//
+//  Created by Aleksandar Vacić on 13.7.15..
+//  Copyright (c) 2015. Radiant Tap. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
